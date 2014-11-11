f313777da4fc1ffa08af85c2bc7c8383d84bb7b7
peppers818 <littlepanniz818@msn.com>
2014-11-10 23:11:32 -0500

    added function for checking if number is prime

--- PrimeNumbers2.py
+++ PrimeNumbers2.py
@@ -7,14 +7,20 @@ input = int(input("Enter a number: "))
 
 # prime number solver function
 def prime(input):
-    for num in range(0, input+1):
-        if num > 1:
-            #Reduced the # of factors looped through
-            #Composite Numbers have a factor <= their sqrt
-            for i in range(2, int(num ** 0.5)+1):
-                if(num % i) == 0:
-                    break
-            else:
-                print(num)
+	for num in range(0, input+1):
+		if num > 1:
+			checkprime(num)
+			
+
+# function that checks for prime number
+def checkprime(num):
+	#Reduced the # of factors looped through
+	#Composite Numbers have a factor <= their sqrt
+	for i in range(2, int(num ** 0.5)+1):
+		if(num % i) == 0:
+			break
+	else:
+		print(num)
+	
 # call prime number solver function				
 prime(input)
